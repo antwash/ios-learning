7669bea2bed7923f2be9ac2b2e74f08faf96da6c
Anthony Washington <awashington251@gmail.com>
2017-03-13 23:28:22 -0500

    Setup collectionViewCell W.I.P

--- Pokedex/Pokedex/MainViewController.swift
+++ Pokedex/Pokedex/MainViewController.swift
@@ -10,13 +10,37 @@ import UIKit
 
 class MainViewController:  UICollectionViewController, UICollectionViewDelegateFlowLayout{
 
+    let cellId = "pokeCell"
+    var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        navigationItem.title = "PokeDex"
-        collectionView?.alwaysBounceVertical = true
-        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
         
+        let background = UIImageView(image: UIImage(named: "bg"))
+            background.contentMode = .scaleAspectFill
+        
+        searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for Pokemon"
+        
+        navigationItem.titleView = searchBar
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.backgroundView = background
+        collectionView?.register(PokemonCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    // sets the width and height of cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
     }
 
 }
