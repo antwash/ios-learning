089f2912302c60fd91b3127011ff3e208ea13532
Anthony Washington <awashington251@gmail.com>
2017-04-20 20:37:58 -0500

    Install Firebase/GeoFiber

---
+++ PokeFinder/PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Anthony Washington on 4/20/17.
+//  Copyright © 2017 Anthony Washington. All rights reserved.
+//
+
+#import "GeoFire.h"
