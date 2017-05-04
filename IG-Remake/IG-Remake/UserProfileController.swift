d2511cc6e7d29a7a56b7f674a789a921a75951b4
Anthony Washington <awashington251@gmail.com>
2017-05-04 10:41:10 -0500

    Add user profile image cells

--- IG-Remake/IG-Remake/UserProfileController.swift
+++ IG-Remake/IG-Remake/UserProfileController.swift
@@ -13,6 +13,7 @@ class UserProfileController: UICollectionViewController,
 UICollectionViewDelegateFlowLayout {
     
     var user: User?
+    let cellID = "imageCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,7 @@ UICollectionViewDelegateFlowLayout {
         collectionView?.register(UserProfileHeader.self,
                                  forSupplementaryViewOfKind: UICollectionElementKindSectionHeader,
                                  withReuseIdentifier: "header")
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
         
         getUser()
     }
@@ -39,11 +41,42 @@ UICollectionViewDelegateFlowLayout {
     }
     
     
+    // header size
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 200)
     }
     
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+            cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
     
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return 13
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    // image cell size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let size = (view.frame.width - 2) / 3
+        return CGSize(width: size, height: size)
+    }
+    
+
     fileprivate func getUser() {
         guard let uuid = FIRAuth.auth()?.currentUser?.uid  else { return }
         
